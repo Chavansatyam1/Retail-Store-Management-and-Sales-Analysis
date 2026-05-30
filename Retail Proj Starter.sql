a24b9f49f6507af269fc7c7e5119d0a17a73df50
Satyam Chavan <140197189+Chavansatyam1@users.noreply.github.com>
2026-05-30 12:34:38 +0530

    upload file

---
+++ Retail Proj Starter.sql
@@ -0,0 +1,12 @@
+-- USE the database
+USE retailproj;
+
+-- Show tables
+SHOW TABLES;
+
+-- View the table
+SELECT * FROM retail;
+
+-- Show count of rows in the table
+SELECT COUNT(*) AS total_rows
+FROM retail;
